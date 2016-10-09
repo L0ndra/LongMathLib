66fa165dddfe19471beb778c07071f87d367753e
LonelyDragon <illialubenets@yandex.ua>
2016-10-10 00:29:48 +0300

    Fix bug in add method

--- LongMath.go
+++ LongMath.go
@@ -2,14 +2,15 @@ package LongMath
 
 func Add(a []int, b []int) []int {
 	maxlen := Max(len(a), len(b))
-
-	sum := make([]int, maxlen+1)
+	a = ChangeLength(a, maxlen)
+	b = ChangeLength(b, maxlen)
+	sum := make([]int, maxlen)
 	carry := 0
 	for i:=0; i<maxlen; i++{
 		sum[i], carry = Adder(a[i], b[i], carry)
 	}
-	if carry != 0 {
-		sum[maxlen] = carry
+	if(carry != 0) {
+		sum = append(sum, carry)
 	}
 	return sum
 }
@@ -26,4 +27,10 @@ func Adder(a, b, prev int) (int, int){
 	sum := a + b + prev
 	return sum % 10, sum / 10
 
+}
+
+func ChangeLength(input []int, len int) []int{
+	output := make([]int, len)
+	copy(output, input)
+	return output
 }
